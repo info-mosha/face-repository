c16878c9305a504d634ca51b56da7ff23060857f
info-mosha <infomosha1@gmail.com>
2026-04-06 19:25:37 +0300

    Update face_service.py

--- backend/face_service.py
+++ backend/face_service.py
@@ -1,1 +1,10 @@
+from deepface import DeepFace
+import numpy as np
 
+def get_embedding(image_path):
+    result = DeepFace.represent(
+        img_path=image_path,
+        model_name="Facenet",
+        enforce_detection=False
+    )
+    return np.array(result[0]["embedding"])
